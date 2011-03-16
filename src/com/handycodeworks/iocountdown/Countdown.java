c850fcd866e147dcbc450e334834dda5e21d5dec
Frank Maker <frank.maker@gmail.com>
2011-03-16 16:24:49 -0700

    Blue balls now show up on click

---
+++ src/com/handycodeworks/iocountdown/Countdown.java
@@ -0,0 +1,208 @@
+package com.handycodeworks.iocountdown;
+
+import static org.anddev.andengine.extension.physics.box2d.util.constants.PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.primitive.Rectangle;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.shape.Shape;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.examples.BaseExample;
+import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
+import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.extension.physics.box2d.util.Vector2Pool;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.texture.BuildableTexture;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.sensor.accelerometer.AccelerometerData;
+import org.anddev.andengine.sensor.accelerometer.IAccelerometerListener;
+import org.anddev.andengine.util.Debug;
+
+import android.hardware.SensorManager;
+import android.widget.Toast;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 18:47:08 - 19.03.2010
+ */
+public class Countdown extends BaseExample implements IAccelerometerListener, IOnSceneTouchListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private Texture mTexture;
+	private BuildableTexture mBuildableTexture;
+
+	private TextureRegion mBall;
+//	private TiledTextureRegion mBoxFaceTextureRegion;
+//	private TiledTextureRegion mCircleFaceTextureRegion;
+//	private TiledTextureRegion mTriangleFaceTextureRegion;
+//	private TiledTextureRegion mHexagonFaceTextureRegion;
+
+	private PhysicsWorld mPhysicsWorld;
+
+	private int mFaceCount = 0;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public Engine onLoadEngine() {
+		Toast.makeText(this, "Touch the screen to add objects.", Toast.LENGTH_LONG).show();
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+		engineOptions.getTouchOptions().setRunOnUpdateThread(true);
+		return new Engine(engineOptions);
+	}
+
+	@Override
+	public void onLoadResources() {
+		/* Textures. */
+		this.mTexture = new Texture(16, 16, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mBuildableTexture = new BuildableTexture(64, 128);
+//		TextureRegionFactory.setAssetBasePath("gfx/");
+
+		/* TextureRegions. */
+//		this.mBoxFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_box_tiled.png", 0, 0, 2, 1); // 64x32
+//		this.mCircleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_circle_tiled.png", 0, 32, 2, 1); // 64x32
+//		this.mTriangleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_triangle_tiled.png", 0, 64, 2, 1); // 64x32
+//		this.mHexagonFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_hexagon_tiled.png", 0, 96, 2, 1); // 64x32
+		TextureRegionFactory.setAssetBasePath("balls/");
+		this.mBall = TextureRegionFactory.createFromAsset(this.mTexture, this, "blue.png", 0, 0); // 64x32
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+
+//		this.enableAccelerometerSensor(this);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene(2);
+		scene.setBackground(new ColorBackground(0, 0, 0));
+		scene.setOnSceneTouchListener(this);
+
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+
+		final Shape ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2);
+		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
+		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
+		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
+
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, wallFixtureDef);
+
+		scene.getFirstChild().attachChild(ground);
+		scene.getFirstChild().attachChild(roof);
+		scene.getFirstChild().attachChild(left);
+		scene.getFirstChild().attachChild(right);
+
+		scene.registerUpdateHandler(this.mPhysicsWorld);
+
+		return scene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		if(this.mPhysicsWorld != null) {
+			if(pSceneTouchEvent.isActionDown()) {
+				this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onAccelerometerChanged(final AccelerometerData pAccelerometerData) {
+		final Vector2 gravity = Vector2Pool.obtain(pAccelerometerData.getY(), pAccelerometerData.getX());
+		this.mPhysicsWorld.setGravity(gravity);
+		Vector2Pool.recycle(gravity);
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	private void addFace(final float pX, final float pY) {
+		final Scene scene = this.mEngine.getScene();
+
+		this.mFaceCount++;
+		Debug.d("Faces: " + this.mFaceCount);
+
+		final Sprite face;
+		final Body body;
+
+		face = new Sprite(pX, pY, this.mBall);
+		body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		/*
+		if(this.mFaceCount % 4 == 0) {
+			face = new AnimatedSprite(pX, pY, this.mBoxFaceTextureRegion);
+			body = PhysicsFactory.createBoxBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		} else if (this.mFaceCount % 4 == 1) {
+			face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion);
+			body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		} else if (this.mFaceCount % 4 == 2) {
+			face = new AnimatedSprite(pX, pY, this.mTriangleFaceTextureRegion);
+			body = Countdown.createTriangleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		} else {
+			face = new AnimatedSprite(pX, pY, this.mHexagonFaceTextureRegion);
+			body = Countdown.createHexagonBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		}*/
+
+//		face.animate(200);
+
+		scene.getLastChild().attachChild(face);
+		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true));
+	}
+	
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
